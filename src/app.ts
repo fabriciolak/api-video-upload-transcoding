01e17555834a000cfba660e1fb32acc3095db950
Fabricio <fabriciolak12@gmail.com>
2025-07-21 13:43:22 -0300

    feat: add graceful shutdown and separate application logic

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import express, { type Application } from 'express'
+import express from 'express'
+import type { Application, Request, Response, NextFunction } from 'express'
+import { type Server } from 'node:http';
 import { config } from './config'
 import routes from './infra/http/routes/index'
 
 class App {
   public app: Application;
+  private server?: Server;
 
   constructor() {
     this.app = express()
@@ -13,12 +16,39 @@ class App {
   private setup(): void {
     this.app.use(express.json())
     this.app.use(routes)
+
+    this.app.use('/uploads', express.static('uploads'));
+
+    // Handle errors
+    this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+      console.error(err.stack)
+      res.status(500).json({
+        message: 'Internal Server Error',
+        error: err.message,
+        code: 'INTERNAL_SERVER_ERROR'
+      })
+    })
   }
 
-  public start(): void {
-    this.app.listen(config.port, () => {
+  public start(): Server {
+    this.server = this.app.listen(config.port, () => {
       console.log(`Server running on port ${config.port}`)
     })
+
+    return this.server;
+  }
+
+  public async close(): Promise<void> {
+    if (this.server) {
+      return new Promise((resolve) => {
+        this.server!.close(() => {
+          console.log('Server closed')
+          resolve()
+        })
+      })
+    } else {
+      console.warn('Server is not running')
+    }
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,43 @@
 import { app } from "./app";
+import { config } from "./config";
 
-(() => {
+async function bootstrap() {
   try {
-    app.start()
+    // TODO: Connect to the database
+    // await connectToDatabase();
+
+    // TODO: Initialize the application with the configuration
+    // await app.init(config);
+
+    const server = app.start()
+
+    const gracefulShutdown = async () => {
+      console.log('Shutting down gracefully...');
+      server.close()
+
+      // TODO: Close database connections
+      // await closeDatabaseConnections();
+
+      process.exit(0);
+    }
+
+    process.on('SIGINT', gracefulShutdown);
+    process.on('SIGTERM', gracefulShutdown);
+
+    process.on('uncaughtException', (err) => {
+      console.error('Uncaught Exception:', err);
+      // gracefulShutdown();
+    });
+
+    process.on('unhandledRejection', (err) => {
+      console.error('Unhandled Rejection:', err);
+      gracefulShutdown();
+    });
+
   } catch (error) {
-    console.log(error)
+    console.error('Failed to start application:', error);
+    process.exit(1);
   }
-})()
+}
+
+bootstrap()
